c8014aa1f8435f357a5b09f2a83a02012b02b0d0
kubaluk <kubaluk757@o2.pl>
2022-09-14 14:15:04 +0200

    backend reads token cookie

--- apps/api/src/auth/jwt.strategy.ts
+++ apps/api/src/auth/jwt.strategy.ts
@@ -7,12 +7,24 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
+      /*jwtFromRequest: ExtractJwt.fromExtractors([
         ExtractJwt.fromAuthHeaderAsBearerToken(),
         (req: Request) => req?.cookies?.["__session"],
-      ]),
-      secretOrKey: "changeMe",
+      ]),*/
+      jwtFromRequest: ExtractJwt.fromExtractors([JwtStrategy.extractJWT]),
+      secretOrKey: "changeThat",
     });
+  }
+
+  private static extractJWT(request: Request): string | null {
+    if (
+      request.cookies &&
+      "token" in request.cookies &&
+      request.cookies.token.length > 0
+    ) {
+      return request.cookies.token;
+    }
+    return null;
   }
 
   async validate(payload: any) {
